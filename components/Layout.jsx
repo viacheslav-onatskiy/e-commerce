8e116d4e011d890cd3d2de1dcc743f72f65a0e48
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-19 12:53:39 -0800

    Added Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,23 @@
-const Layout = () => {
-  return <div></div>;
+import Head from 'next/head';
+import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Viacheslav's Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Layout } from '@/components';
 import './globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
