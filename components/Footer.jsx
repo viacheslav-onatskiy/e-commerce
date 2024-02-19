362733a66d616cb4c8a3f9a7ce5b45019b3f6321
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-19 12:54:38 -0800

    Added Footer, FooterBanner and Navbar components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,15 @@
-const Footer = () => {
-  return <div></div>;
-};
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2024 JSM Keyboards All rights reserved</p>
+      <p className="icons">
+        <AiFillGithub />
+        <AiFillLinkedin />
+      </p>
+    </div>
+  );
+};
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineShopping } from 'react-icons/ai';
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Keyboards</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => {}}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
