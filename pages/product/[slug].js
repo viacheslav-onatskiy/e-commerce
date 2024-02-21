5590860d928cbccc8f8c88c60f4f947501686a60
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-20 21:13:54 -0800

    Handle click on Buy Now button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,9 +11,15 @@ import {
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
-  const { qty, incQty, decQty, onAdd } = useStateContext();
 
+  const { qty, incQty, decQty, onAdd, setShowCart } = useStateContext();
   const [index, setIndex] = useState(0);
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -74,7 +80,7 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick={() => {}}>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
